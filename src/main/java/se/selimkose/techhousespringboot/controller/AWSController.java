a2f24abf10c10226567ce11f84aaeff4408784be
selim <selim-kose@live.com>
2024-11-06 11:53:34 +0100

    Add s3 download and upload

--- src/main/java/se/selimkose/techhousespringboot/controller/AWSController.java
+++ src/main/java/se/selimkose/techhousespringboot/controller/AWSController.java
@@ -1,13 +1,11 @@
 package se.selimkose.techhousespringboot.controller;
 
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import se.selimkose.techhousespringboot.service.S3Service;
+import java.io.File;
+import java.io.IOException;
 
 @AllArgsConstructor
 @RestController
@@ -17,11 +15,24 @@ public class AWSController {
 
     private S3Service s3Service;
 
-
     @GetMapping("/aws")
     public String listAllBuckets() {
         s3Service.listAllBuckets();
         s3Service.listBucketObjects();
         return ("Hello from AWSController");
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+        File file = s3Service.convertMultiPartToFile(multipartFile);
+        String message = s3Service.uploadFile(multipartFile.getOriginalFilename(), file);
+        file.delete();
+        return message;
+    }
+
+    @GetMapping("/download/{key}")
+    public String downloadFile(@PathVariable("key") String key) {
+        s3Service.downloadFile(key);
+        return "File downloaded successfully";
     }
 }
--- src/main/java/se/selimkose/techhousespringboot/service/S3Service.java
+++ src/main/java/se/selimkose/techhousespringboot/service/S3Service.java
@@ -3,13 +3,16 @@ package se.selimkose.techhousespringboot.service;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.Bucket;
-import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
-import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.*;
 import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
 
 @AllArgsConstructor
@@ -24,10 +27,11 @@ public class S3Service {
     public void listAllBuckets() {
         ListBucketsResponse response = s3Client.listBuckets();
         List<Bucket> bucketList = response.buckets();
-        for (Bucket bucket: bucketList) {
-            System.out.println("Bucket name "+bucket.name());
+        for (Bucket bucket : bucketList) {
+            System.out.println("Bucket name " + bucket.name());
         }
     }
+
     public void getObject(String key) {
         s3Client.getObject(builder -> builder.bucket(bucketName).key(key));
     }
@@ -47,6 +51,41 @@ public class S3Service {
         } catch (S3Exception e) {
             System.err.println(e.awsErrorDetails().errorMessage());
             System.exit(1);
+        }
+    }
+
+    public String uploadFile(String key,File file) {
+        try {
+            s3Client.putObject(PutObjectRequest.builder()
+                            .bucket(bucketName)
+                            .key(key)
+                            .build(),
+                    RequestBody.fromFile(file));
+
+            return "File uploaded successfully!";
+        } catch (S3Exception e) {
+            throw new RuntimeException("Error uploading file to S3: " + e.getMessage(), e);
+        }
+    }
+
+    public File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convFile)) {
+            fos.write(file.getBytes());
+        }
+        return convFile;
+    }
+
+    public File downloadFile(String key) {
+        try {
+            File downloadFile = Paths.get("downloaded-" + key).toFile();
+            s3Client.getObject(GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build(), downloadFile.toPath());
+            return downloadFile;
+        } catch (S3Exception e) {
+            throw new RuntimeException("Error downloading file from S3: " + e.getMessage(), e);
         }
     }
 }
